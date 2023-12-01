e7cee86d7da1a5478e4def4fd693b02c67f88daf
Sarthak <65298686+SartMa@users.noreply.github.com>
2023-12-01 17:54:00 +0530

    removed redundant directory

--- level_data/1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="git" name="1" tilewidth="16" tileheight="16" tilecount="6" columns="3">
- <image source="../graphics/enemy/run/1.png" width="51" height="46"/>
-</tileset>
--- level_data/tile0001.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="git" name="tile000" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../graphics/chest/tile000.png" width="48" height="32"/>
-</tileset>
